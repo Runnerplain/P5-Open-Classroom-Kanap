9acfbdd320932a9facd7a9b75707c6d853179df9
Orten Runnerplain <runnerplain@gmail.com>
2023-01-18 11:39:36 +0100

    add Article cart

---
+++ js/cart.js
@@ -0,0 +1,113 @@
+const cart = []
+
+retrieveItemFromCache()
+console.log(cart)
+cart.forEach((item) => displayItem(item))
+
+function retrieveItemFromCache() {
+  const numberOfItems = localStorage.length
+  for (let i = 0; i < numberOfItems; i++) {
+    const item = localStorage.getItem(localStorage.key(i)) || ''
+    const itemObjet = JSON.parse(item)
+    cart.push(itemObjet)
+  }
+}
+
+function displayItem(item) {
+  const article = makeArticle(item)
+  const imageDiv = makeImageDiv(item)
+  article.appendChild(imageDiv)
+
+  const card__Item__Content = makeCartContent(item)
+  article.appendChild(card__Item__Content)
+  displayArticle(article)
+  displayTotalQuantity(item)
+}
+
+function displayTotalQuantity(item) {
+  const totalQuantity = document.querySelector('#totalQuantity')
+  totalQuantity.textContent = item.quantity
+}
+
+function makeCartContent(item) {
+  const cardItemContent = document.createElement('div')
+  cardItemContent.classList.add('cart__item__content')
+  const description = makeDescription(item)
+  const settings = makeSettings(item)
+
+  cardItemContent.appendChild(description)
+  cardItemContent.appendChild(settings)
+  return cardItemContent
+  // cardItemContent.appendChild(settings)
+}
+
+function makeSettings(item) {
+  const settings = document.createElement('div')
+  settings.classList.add('cart__item__content__settings')
+  addQuantityToSettings(settings, item)
+  addDeleteToSettings(settings)
+  return settings
+}
+
+function addDeleteToSettings(settings) {
+  const div = document.createElement('div')
+  div.classList.add('cart__item__content__settings__delete')
+  const p = document.createElement('p')
+  p.textContent = 'Supprimer'
+  div.appendChild(p)
+  settings.appendChild(div)
+}
+
+function addQuantityToSettings(settings, item) {
+  const quantity = document.createElement('div')
+  quantity.classList.add('cart__item__content__settings__quantity')
+  const p = document.createElement('p')
+  p.textContent = 'Qté : '
+  quantity.appendChild(p)
+  const input = document.createElement('input')
+  input.type = 'number'
+  input.classList.add('itemQuantity')
+  input.name = 'itemQuantity'
+  input.min = '1'
+  input.max = '100'
+  input.value = item.quantity
+  quantity.appendChild(input)
+  settings.appendChild(quantity)
+}
+
+function makeDescription(item) {
+  const description = document.createElement('div')
+  description.classList.add('cart__item__content__description')
+  const h2 = document.createElement('h2')
+  h2.textContent = item.name
+  const p = document.createElement('p')
+  p.textContent = item.color
+  const p2 = document.createElement('p')
+  p2.textContent = item.price + ' €'
+  description.appendChild(h2)
+  description.appendChild(p)
+  description.appendChild(p2)
+  return description
+}
+
+function displayArticle(article) {
+  document.querySelector('#cart__items').appendChild(article)
+}
+
+function makeArticle(item) {
+  const article = document.createElement('article')
+  article.classList.add('card__item')
+  article.dataset.id = item.id
+  article.dataset.color = item.color
+  return article
+}
+
+function makeImageDiv(item) {
+  const div = document.createElement('div')
+  div.classList.add('cart__item__img')
+  const image = document.createElement('img')
+  image.src = item.imageUrl
+  image.alt = item.altTxt
+  div.appendChild(image)
+  return div
+}
--- js/product.js
+++ js/product.js
@@ -1,99 +1,94 @@
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
-const id = urlParams.get("id")
+const id = urlParams.get('id')
 if (id != null) {
-    let itemPrice = 0
-    let imgUrl, altText
+  let itemPrice = 0
+  let imgUrl, altText, articleName
 }
 
-
 fetch(`http://localhost:3000/api/products/${id}`)
-.then(response => response.json())
-.then((res) => handleData(res))
+  .then((response) => response.json())
+  .then((res) => handleData(res))
 
 function handleData(kanap) {
-
-    const { altTxt, colors, description, imageUrl, name, price } = kanap
-    itemPrice = price
-    makeImage(imageUrl, altTxt)
-    imgUrl = imageUrl
-    altText = altTxt
-    makeTitle(name)
-    makePrice(price)
-    makeDescription(description)
-    makeColors(colors)
-    make
+  const { altTxt, colors, description, imageUrl, name, price } = kanap
+  itemPrice = price
+  makeImage(imageUrl, altTxt)
+  imgUrl = imageUrl
+  altText = altTxt
+  articleName = name
+  makeTitle(name)
+  makePrice(price)
+  makeDescription(description)
+  makeColors(colors)
+  make
 }
 
 function makeImage(imageUrl, altTxt) {
-    const image = document.createElement('img')
-    image.src = imageUrl
-    image.alt = altTxt
-    const parent = document.querySelector(".item__img")
-    if (parent != null) parent.appendChild(image)
+  const image = document.createElement('img')
+  image.src = imageUrl
+  image.alt = altTxt
+  const parent = document.querySelector('.item__img')
+  if (parent != null) parent.appendChild(image)
 }
 
 function makeTitle(name) {
-    const h1 = document.querySelector("#title")
-    if (h1 != null) h1.textContent = name
+  const h1 = document.querySelector('#title')
+  if (h1 != null) h1.textContent = name
 }
 
 function makePrice(price) {
-    const span = document.querySelector("#price")
-    if (span != null) span.textContent = price
+  const span = document.querySelector('#price')
+  if (span != null) span.textContent = price
 }
 
 function makeDescription(description) {
-    const p = document.querySelector("#description")
-    if (p != null) p.textContent = description
+  const p = document.querySelector('#description')
+  if (p != null) p.textContent = description
 }
 
 function makeColors(colors) {
-    const select = document.querySelector("#colors")
-    if (select != null) {
-        colors.forEach((color) => {
-            const option = document.createElement("option")
-            option.value = color
-            option.textContent = color
-            select.appendChild(option)
-        })
-    }
+  const select = document.querySelector('#colors')
+  if (select != null) {
+    colors.forEach((color) => {
+      const option = document.createElement('option')
+      option.value = color
+      option.textContent = color
+      select.appendChild(option)
+    })
+  }
 }
 
-const button = document.querySelector("#addToCart")
-if (button != null) {
-    button.addEventListener("click", handleClick) 
-}
+const button = document.querySelector('#addToCart')
+button.addEventListener('click', handleClick)
 
 function handleClick() {
-    const color = document.querySelector("#colors").value
-    const quantity = document.querySelector("#quantity").value
-    if (isOrderInvalid(color, quantity)) return
-    saveCart(color, quantity)
-    redirectToCart()
+  const color = document.querySelector('#colors').value
+  const quantity = document.querySelector('#quantity').value
+  if (isOrderInvalid(color, quantity)) return
+  saveOrder(color, quantity)
+  redirectToCart()
 }
 
-function saveCart(color, quantity) {
-    const data =  {
-        id: id,
-        color: color,
-        quantity: Number(quantity),
-        price: itemPrice,
-        imageUrl: imgUrl,
-        altTxt: altText
-
-    }
-    localStorage.setItem(id, JSON.stringify(data))
-
+function saveOrder(color, quantity) {
+  const data = {
+    id: id,
+    color: color,
+    quantity: Number(quantity),
+    price: itemPrice,
+    imageUrl: imgUrl,
+    altTxt: altText,
+    name: articleName,
+  }
+  localStorage.setItem(id, JSON.stringify(data))
 }
 
 function isOrderInvalid(color, quantity) {
-    if (color == null || color === "" || quantity == null || quantity == 0) {
-        alert("S'il vous plait, choisissez une couleur et une quantité")
-        return true
-    }
-
+  if (color == null || color === '' || quantity == null || quantity == 0) {
+    alert("S'il vous plait, choisissez une couleur et une quantité")
+    return true
+  }
 }
 function redirectToCart() {
-    window.location.href = "cart.html"
+  window.location.href = 'cart.html'
 }
